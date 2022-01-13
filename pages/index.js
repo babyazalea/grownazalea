70af17c9681094dd1a04897cdfa575cfda11d3eb
babyazalea <usun16@gmail.com>
2022-01-13 11:33:39 +0900

    editing text

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h2>hello grownazalea</h2>
+        <h2>grownazalea</h2>
       </main>
     </div>
   );
