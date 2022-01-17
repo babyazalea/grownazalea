64adc5e86d0ae2045eae1926c28e4f71549780ca
babyazalea <usun16@gmail.com>
2022-01-17 17:54:02 +0900

    styling layout & header

--- components/header.js
+++ components/header.js
@@ -1,53 +1,29 @@
+import Link from "next/link";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
+import styles from "../styles/header.module.css";
+
 export default function Header() {
   return (
-    <header>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse flex-column-sm align-items-center"
-            id="navbarTogglerDemo01"
-          >
-            <a
-              className="navbar-brand me-auto d-flex flex-column align-items-lg-center"
-              href="#"
-            >
-              <span className="font-monospace fst-italic">grownazalea</span>
-            </a>
-            <ul className="navbar-nav mb-2 mb-lg-0 justify-content-end align-items-lg-center gap-lg-4">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  style={{ cursor: "pointer", fontSize: "1.5rem" }}
-                >
-                  <FontAwesomeIcon icon={faGithub} />
-                </a>
-              </li>
-            </ul>
-          </div>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className="logo">grownazalea</div>
+        <div className={styles.menus}>
+          <ul>
+            <li>
+              <Link href={"/"}>resume</Link>
+            </li>
+            <li>
+              <Link href="/portfolio">portfolio</Link>
+            </li>
+            <li>
+              <a href="https://github.com/babyazalea">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </header>
