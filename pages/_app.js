757302d691bd91005a2bbd2a2a2c85173e95da18
babyazalea <usun16@gmail.com>
2022-01-17 16:55:54 +0900

    remove bootstrap package

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,10 @@
-import { useEffect } from "react";
-
 import "../styles/globals.css";
-import "bootstrap/dist/css/bootstrap.css";
+
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    typeof document !== undefined
-      ? require("bootstrap/dist/js/bootstrap")
-      : null;
-  }, []);
-
   return <Component {...pageProps} />;
 }
 
