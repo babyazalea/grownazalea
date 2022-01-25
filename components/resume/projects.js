8cbf51c55912008c16ef13aafa427724a7bb0a7a
babyazalea <usun16@gmail.com>
2022-01-26 08:23:04 +0900

    modified load projects

--- components/resume/projects.js
+++ components/resume/projects.js
@@ -2,14 +2,20 @@ import { useEffect } from "react";
 import styles from "../../styles/projects.module.css";
 
 export default function Projects({ projects }) {
+  // useEffect(() => {
+  //   console.log(projects);
+  // }, [projects]);
+
   return (
     <div className={styles.projects}>
       <span>projects</span>
       <ul>
-        {projects.map(({ id, title, date }) => (
+        {projects.map(({ id, title, date, skills, description }) => (
           <li key={id}>
             <span>{title}</span>
             <span>{date}</span>
+            <span>{skills}</span>
+            <span>{description}</span>
           </li>
         ))}
       </ul>
