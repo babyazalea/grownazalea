235839708b73db1bc57d4b51bfe9c8b44b94d198
babyazalea <usun16@gmail.com>
2022-02-08 17:35:39 +0900

    added open props cdn

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
+import { Fragment } from "react";
+import Head from "next/head";
+
 import Layout from "../components/layout";
 import Resume from "../components/resume/resume";
 
 import { getSortedProjectsData } from "../lib/projects";
 
+import "https://unpkg.com/open-props";
+import "https://unpkg.com/open-props/normalize.min.css";
+
 export default function Home({ allProjectsData }) {
   return (
-    <Layout>
-      <Resume allProjectsData={allProjectsData} />
-    </Layout>
+    <Fragment>
+      <Head>
+        <title>grownazalea</title>
+        <link rel="stylesheet" href="https://unpkg.com/open-props" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/open-props/normalize.min.css"
+        />
+      </Head>
+      <Layout>
+        <Resume allProjectsData={allProjectsData} />
+      </Layout>
+    </Fragment>
   );
 }
 
