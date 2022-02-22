7a7a42e642a2a23a6ac808a83b38fd34186d3cbf
babyazalea <usun16@gmail.com>
2022-02-23 03:02:14 +0900

    added: learning part

--- components/resume/skills.js
+++ components/resume/skills.js
@@ -6,7 +6,9 @@ import styles from "../../styles/skills.module.css";
 export default function Skills() {
   return (
     <div className={styles.skills}>
-      <h2>Skills</h2>
+      <h2 className={styles["skills-title"]}>
+        Skills <span>(ft. Learning)</span>
+      </h2>
       <hr />
       <div>
         <div className={`${styles["skills-large-classification"]} basement`}>
@@ -32,7 +34,11 @@ export default function Skills() {
                 합니다. CSS만을 이용한 반응형 웹 스타일링을 할 수 있습니다.
               </li>
               <li>
-                Bootstrap과 같은 CSS Framework 사용 경험이 있으며, 요즘은 <span className={styles["underline-emphasis"]}>Tailwindcss를 사이드 프로젝트에 적극적으로 활용</span>하고 있습니다.
+                Bootstrap과 같은 CSS Framework 사용 경험이 있으며, 요즘은{" "}
+                <span className={styles["underline-emphasis"]}>
+                  Tailwindcss를 사이드 프로젝트에 적극적으로 활용
+                </span>
+                하고 있습니다.
               </li>
             </ul>
           </div>
@@ -49,11 +55,12 @@ export default function Skills() {
                 </span>
                 <ul>
                   <li>
-                    let, const 각 변수 선언 방식의 특징을 알고 적절하게 사용합니다.
+                    let, const 각 변수 선언 방식의 특징을 알고 적절하게
+                    사용합니다.
                   </li>
                   <li>
-                    자바스크립트의 데이터 타입과 타입의 분류에 따른 윈시
-                    타입과 객체 타입의 차이점을 알고, 활용할 수 있습니다.
+                    자바스크립트의 데이터 타입과 타입의 분류에 따른 윈시 타입과
+                    객체 타입의 차이점을 알고, 활용할 수 있습니다.
                   </li>
                   <li>
                     자바스크립트 언어의 암묵적 타입 변환이 일어나는 경우를 알고
@@ -62,8 +69,10 @@ export default function Skills() {
                   <li>
                     렉시컬 스코프, Temporal Dead Zone(TDZ), 변수 & 함수 호이스팅
                     등 자바스크립트 언어의 특이점(?)을 알고 있습니다. (이런
-                    특이점 때문에 발생하는 에러에 대한 경험이 있고, 에러가 발생하지 않으려면
-                    코드를 어떻게 짜야하는지 대해 배웠으며, 여러 개발 도서를 통해 심도 있는 지식을 쌓으려 노력 중입니다.)
+                    특이점 때문에 발생하는 에러에 대한 경험이 있고, 에러가
+                    발생하지 않으려면 코드를 어떻게 짜야하는지 대해 배웠으며,
+                    여러 개발 도서를 통해 심도 있는 지식을 쌓으려 노력
+                    중입니다.)
                   </li>
                 </ul>
               </li>
@@ -96,7 +105,8 @@ export default function Skills() {
               </li>
               <li className={styles["underline-emphasis"]}>
                 useState, useEffect, Context API(useContext) 등 16.8버전 이후로
-                추가된 React의 기능(React Hooks)으로 프로젝트를 제작할 수 있습니다.
+                추가된 React의 기능(React Hooks)으로 프로젝트를 제작할 수
+                있습니다.
               </li>
               <li className={styles["underline-emphasis"]}>
                 Gatsby.js, Next.js를 이용해서 정적 사이트를 만들었습니다.
@@ -132,8 +142,64 @@ export default function Skills() {
                   <a href="https://babyazalea.notion.site/Flutter-0f1a04b91c2b464583a16b31250359ad">
                     Go!
                   </a>
+                  )
                 </div>
-                )
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles["skills-middle-classification"]}>
+          <h3>Learning</h3>
+          <div className={styles["skills-middle-classification"]}>
+            <h4>오프라인</h4>
+            <ul>
+              <li>
+                2018-10 ~ 2019-04 : ⌜웹 & 앱 프론트엔드⌟ 국비지원 과정 수료.
+              </li>
+              <li>2021 ~ 현재 : 한국방송통신대학 컴퓨터학과 3학년 재학 중.</li>
+            </ul>
+          </div>
+          <div className={styles["skills-middle-classification"]}>
+            <h4>온라인</h4>
+            <ul>
+              <li
+                className={`${styles["skills-small-classification"]} ${styles.udemy}`}
+              >
+                <span>수강 완료한 Udemy Courses:</span>
+                <ul>
+                  <li>
+                    <a href="https://www.udemy.com/course/react-the-complete-guide-incl-redux/">
+                      React - The Complete Guide (incl Hooks, React Router,
+                      Redux)
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.udemy.com/course/react-nodejs-express-mongodb-the-mern-fullstack-guide/">
+                      React, NodeJS, Express & MongoDB - The MERN Fullstack
+                      Guide
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.udemy.com/course/the-complete-guide-to-angular-2/">
+                      Angular - The Complete Guide
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.udemy.com/course/angular-2-and-nodejs-the-practical-guide/">
+                      Angular & NodeJS - The MEAN Stack Guide
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.udemy.com/course/vuejs-2-the-complete-guide/">
+                      Vue - The Complete Guide (incl. Router & Composition API)
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/">
+                      Flutter & Dart - The Complete Guide
+                    </a>
+                  </li>
+                </ul>
               </li>
             </ul>
           </div>
