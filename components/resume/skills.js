75ee655609505ccf882e6e284d31ed61c4c1cf54
tyange <usun16@gmail.com>
2022-03-08 09:23:22 +0900

    fixed: content

--- components/resume/skills.js
+++ components/resume/skills.js
@@ -18,14 +18,7 @@ export default function Skills() {
           >
             <h4>HTML & CSS</h4>
             <ul>
-              <li>
-                HTML의 시멘틱 태그를 이용해 웹의 구조를 만들 수 있으며 이를
-                토대로{" "}
-                <span className={styles["underline-emphasis"]}>
-                  웹 표준을 준수
-                </span>
-                하는 웹을 설계할 수 있습니다.
-              </li>
+              <li>마크업 개발을 위한 HTML 사용 가능.</li>
               <li>
                 <span className={styles["underline-emphasis"]}>
                   CSS 문법을 올바른 방법으로 구사할 줄 압니다. flexbox, media
@@ -36,58 +29,31 @@ export default function Skills() {
               <li>
                 Bootstrap과 같은 CSS Framework 사용 경험이 있으며, 요즘은{" "}
                 <span className={styles["underline-emphasis"]}>
-                  Tailwindcss를 사이드 프로젝트에 적극적으로 활용
+                  Tailwindcss를 사이드 프로젝트에 적극적으로 쓰고 있어요.
                 </span>
-                하고 있습니다.
               </li>
             </ul>
           </div>
           <div
             className={`${styles["skills-middle-classification"]} javascript`}
           >
-            <h4>Javascript</h4>
+            <h4>Javascript(Node.js)</h4>
             <ul>
               <li
                 className={`${styles["skills-small-classification"]} ${styles["syntax"]}`}
               >
-                <span className={styles["underline-emphasis"]}>
-                  문법(~ES6)에 대한 충분한 이해:
+                <span>
+                  개발에 필요한 자바스크립트 문법(~ES6까지의 문법과 실행
+                  컨텍스트 등)을 적절히 활용할 수 있음.
                 </span>
-                <ul>
-                  <li>
-                    let, const 각 변수 선언 방식의 특징을 알고 적절하게
-                    사용합니다.
-                  </li>
-                  <li>
-                    자바스크립트의 데이터 타입과 타입의 분류에 따른 윈시 타입과
-                    객체 타입의 차이점을 알고, 활용할 수 있습니다.
-                  </li>
-                  <li>
-                    자바스크립트 언어의 암묵적 타입 변환이 일어나는 경우를 알고
-                    예측 할 수 있습니다.
-                  </li>
-                  <li>
-                    렉시컬 스코프, Temporal Dead Zone(TDZ), 변수 & 함수 호이스팅
-                    등 자바스크립트 언어의 특이점(?)을 알고 있습니다. (이런
-                    특이점 때문에 발생하는 에러에 대한 경험이 있고, 에러가
-                    발생하지 않으려면 코드를 어떻게 짜야하는지 대해 배웠으며,
-                    여러 개발 도서를 통해 심도 있는 지식을 쌓으려 노력
-                    중입니다.)
-                  </li>
-                </ul>
               </li>
-              <li className={`${styles["skills-small-classification"]} nodejs`}>
-                <span>Node.js:</span>
-                <ul>
-                  <li>
-                    npm, yarn과 같은 패키지 매니저로 필요한 패키지를 설치하고
-                    때에 따라 패키지(들)의 버전 관리를 할 수 있습니다.
-                  </li>
-                  <li>
-                    Express.js, mongoose(MongoDB ORM)를 이용하여 백엔드 환경을
-                    구성하고 이를 사이드 프로젝트에 적용해본 경험이 있습니다.
-                  </li>
-                </ul>
+              <li>
+                npm, yarn과 같은 패키지 매니저로 필요한 패키지를 설치하고 때에
+                따라 패키지(들)의 버전 관리를 할 수 있습니다.
+              </li>
+              <li>
+                Express.js, mongoose(MongoDB ORM)를 이용하여 백엔드 환경을
+                구성하고 이를 사이드 프로젝트에 적용해본 경험이 있습니다.
               </li>
             </ul>
           </div>
@@ -110,6 +76,19 @@ export default function Skills() {
               </li>
               <li className={styles["underline-emphasis"]}>
                 Gatsby.js, Next.js를 이용해서 정적 사이트를 만들었습니다.
+              </li>
+              <li>
+                <span
+                  className={styles["underline-emphasis"]}
+                  style={{ marginRight: "0.5rem" }}
+                >
+                  공유 가계부 웹-앱 버전(Next.js, Redux...)을 만들고 있습니다.
+                </span>
+                (진행 상황 보러가기
+                <div className={styles["ttattogat-link"]}>
+                  <FontAwesomeIcon icon={faHandPointRight} />
+                  <a href="https://github.com/tyange/ttattogat">Go!</a>)
+                </div>
               </li>
             </ul>
           </div>
