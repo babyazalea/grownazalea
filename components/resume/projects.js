10ea88d6b31b6c242bd762f528da2da455b58f73
tyange <usun16@gmail.com>
2022-05-31 13:49:21 +0900

    포트폴리오 구글 드라이브 링크 추가

--- components/resume/projects.js
+++ components/resume/projects.js
@@ -1,11 +1,19 @@
 import ProjectCard from "../project-card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "../../styles/projects.module.css";
 
 export default function Projects({ projects }) {
   return (
     <div className={styles.projects}>
-      <h2>대표적인 프로젝트</h2>
+      <div className={styles.projectsHeader}>
+        <h2>대표적인 프로젝트</h2>
+        <a href="https://drive.google.com/file/d/1jgfVi_4bC1kpdkPz4gc8XFNsIsPlxMVQ/view?usp=sharing">
+          <span>더 자세히 보기</span>
+          <FontAwesomeIcon icon={faLink} size="xs" />
+        </a>
+      </div>
       <hr />
       <ul className={styles.projectsCardUl}>
         {projects.map((project) => (
